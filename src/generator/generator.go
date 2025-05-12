ac3cb52068b8eba457f1d33175606797c9ff0856
CM <cmaraziaris@student.ethz.ch>
2025-05-12 11:14:41 +0200

    Add NextBatchUID to create batches containing only UID statements; Rename NextBatch to NextBatchRandom

--- src/generator/generator.go
+++ src/generator/generator.go
@@ -19,7 +19,7 @@ func (g *generator) Next() string {
 
 // generates a batch of random sql queries.
 // each batch creates a new table
-func (g *generator) NextBatch(n uint) []ast.Prod {
+func (g *generator) NextBatchRandom(n uint) []ast.Prod {
 	batch := make([]ast.Prod, n)
 
 	sch := generateTable(1)
@@ -31,6 +31,23 @@ func (g *generator) NextBatch(n uint) []ast.Prod {
 			StmtSeq: make(map[string]uint),
 		}
 		batch[i] = GenerateStatement(nil, s)
+	}
+
+	return batch
+}
+
+func (g *generator) NextBatchUID(n uint) []ast.Prod {
+	batch := make([]ast.Prod, n)
+
+	sch := generateTable(1)
+
+	for i := range n {
+		s := &ast.Scope{
+			Tables:  sch.Tables,
+			Schema:  sch,
+			StmtSeq: make(map[string]uint),
+		}
+		batch[i] = GenerateIUD(nil, s)
 	}
 
 	return batch
