d8065274a2af3dae55355c5af7484323c6a5997a
CM <cmaraziaris@student.ethz.ch>
2025-05-12 11:17:32 +0200

    Add error reporting in crash

--- src/fuzzer/batch.go
+++ src/fuzzer/batch.go
@@ -9,12 +9,14 @@ import (
 type Batch struct {
 	schema string
 	stmts  []ast.Prod
+	err    error
 }
 
 func NewBatch(sch string, stmts []ast.Prod) *Batch {
 	return &Batch{
 		schema: sch,
 		stmts:  stmts,
+		err:    nil,
 	}
 }
 
@@ -31,7 +33,16 @@ func (b *Batch) Len() int {
 
 func (b *Batch) String(out strings.Builder) string {
 	sch := b.schema
+	out.WriteString(sch + "\n")
+	for _, s := range b.stmts {
+		out.WriteString(s.Out() + "\n;")
+	}
+	return out.String()
+}
 
+func (b *Batch) CrashString(out strings.Builder) string {
+	sch := b.schema
+	out.WriteString("/* -----------Error-----------\n" + b.err.Error() + "-----------/Error----------- */\n")
 	out.WriteString(sch + "\n")
 	for _, s := range b.stmts {
 		out.WriteString(s.Out() + "\n;")
