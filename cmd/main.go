17db620654457ec05e9cef744c579154b3682583
Cnordg <carlhenrikklaes.nordgren@uzh.ch>
2025-05-13 20:54:06 +0200

    Added timeout to fuzzer CLI
    Right now we dont do any cleaning up so its not
    ideal way of stopping fuzzer

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log/slog"
 	"os"
+	"os/signal"
 	"runtime"
+	"syscall"
+	"time"
 
 	"github.com/cnordg/ast-group-project/src/fuzzer"
 )
@@ -20,6 +24,7 @@ func main() {
 	debugMode := flag.Bool("debug", false, "Run in debug mode with verbose output")
 	threads := flag.Int("t", runtime.NumCPU(), "Number of threads to use for fuzzing")
 	batchSize := flag.Uint("b", defaultBatchSize, "Number of queries per batch")
+	timeout := flag.Duration("d", 0, "Duration to run the fuzzer (e.g. '10m', '1h')")
 	outputDir := flag.String("o", defaultOutDir, "Directory to save bug reports")
 	help := flag.Bool("h", false, "Show help message")
 	flag.Parse()
@@ -38,6 +43,10 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Create a context that will be canceled on SIGINT or SIGTERM
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
 	// run fuzzer
 	cfg := &fuzzer.Config{
 		Debug:     *debugMode,
@@ -54,7 +63,20 @@ func main() {
 		"outputDir", *outputDir)
 
 	l.Info("Fuzzer running... press Ctrl+C to stop.")
-	f.Fuzz()
+	go f.Fuzz()
+
+	// If timeout is specified, set up a timer
+	var timeoutChan <-chan time.Time
+	if *timeout > 0 {
+		timeoutChan = time.After(*timeout)
+	}
+
+	select {
+	case <-ctx.Done():
+		l.Info("Interrupt received..")
+	case <-timeoutChan:
+		l.Info("Timeout reached...")
+	}
 }
 
 func printUsage() {
@@ -65,6 +87,7 @@ func printUsage() {
 	fmt.Println("  -debug            Run in debug mode with verbose output")
 	fmt.Println("  -t <num>          Number of threads to use (default: number of CPU cores)")
 	fmt.Println("  -b <num>          Number of queries per batch (default: 100)")
+	fmt.Println("  -d <dur>          Duration to run fuzzer (e.g. '10m', '1h', '24h'). 0 means run indefinitely")
 	fmt.Println("  -o <directory>    Directory to save bug reports (default: ./bugs)")
 	fmt.Println("  -h                Show this help message")
 	fmt.Println("\nExamples:")
