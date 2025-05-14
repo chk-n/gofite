3bee895a6805a44de2c3828cc2d9ab53165baf15
CM <cmaraziaris@student.ethz.ch>
2025-05-14 15:48:14 +0200

    Add 'Structured' Batch generation -- 5 UID then random

--- src/generator/generator.go
+++ src/generator/generator.go
@@ -52,3 +52,32 @@ func (g *generator) NextBatchUID(n uint) []ast.Prod {
 
 	return batch
 }
+
+var uid_statements uint = 5
+
+func (g *generator) NextBatchStructured(n uint) []ast.Prod {
+	assert(n > uid_statements, "Batch not large enough")
+	batch := make([]ast.Prod, n)
+
+	sch := GenerateTable(1)
+	var i uint
+	for i = range uid_statements {
+		s := &ast.Scope{
+			Tables:  sch.Tables,
+			Schema:  sch,
+			StmtSeq: make(map[string]uint),
+		}
+		batch[i] = GenerateIUD(nil, s)
+	}
+
+	for ; i < n; i++ {
+		s := &ast.Scope{
+			Tables:  sch.Tables,
+			Schema:  sch,
+			StmtSeq: make(map[string]uint),
+		}
+		batch[i] = GenerateStatement(nil, s)
+	}
+
+	return batch
+}
